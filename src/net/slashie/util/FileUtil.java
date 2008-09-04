3b4419546a7691f2e2e3c47e455f4b983732490b
java.koder <java.koder@997be215-1356-0410-a312-4b11603a79ba>
2008-09-04 19:25:05 +0000

    Added documentation

--- src/net/slashie/util/FileUtil.java
+++ src/net/slashie/util/FileUtil.java
@@ -1,39 +1,77 @@
-package net.slashie.util; //test edit
+package net.slashie.util;
 
 import java.io.*;
 
 public class FileUtil {
-
-    public static void deleteFile(String what){
-    	new File(what).delete();
+	/**
+	 * Deletes the specified file
+	 * @param fileName The file to be deleted
+	 */
+    public static void deleteFile(String fileName){
+    	new File(fileName).delete();
+    }
+    
+    /**
+     * Duplicates a file contents into another file
+     * 
+     * @param source The file to take the data from
+     * @param destination The filename to write the data to
+     * @throws IOException 
+     */
+    public static void copyFile(File source, File destination) throws IOException{
+    	FileInputStream fis  = new FileInputStream(source);
+    	FileOutputStream fos = new FileOutputStream(destination);
+    	byte[] buf = new byte[1024];
+    	int i = 0;
+    	while((i=fis.read(buf))!=-1) {
+    		fos.write(buf, 0, i);
+    	}
+    	fis.close();
+    	fos.close();
     }
 
-    public static void copyFile(File origen, File destino) throws Exception {
-        FileInputStream fis  = new FileInputStream(origen);
-        FileOutputStream fos = new FileOutputStream(destino);
-        byte[] buf = new byte[1024];
-        int i = 0;
-        while((i=fis.read(buf))!=-1) {
-          fos.write(buf, 0, i);
-          }
-        fis.close();
-        fos.close();
-    }
-
+    /**
+     * Acquires a Buffered reader usable to read the contents of a file
+     * 
+     * @param fileName Name of the file that will be read
+     * @return The BufferedReader
+     * @throws IOException
+     */
     public static BufferedReader getReader(String fileName) throws IOException{
         BufferedReader x = new BufferedReader(new FileReader(fileName));
 		return x;
 	}
 
+    /**
+     * Acquires a Buffered Writer usable to write a new file
+     * 
+     * @param fileName The name of the file to be written
+     * @return
+     * @throws IOException
+     */
 	public static BufferedWriter getWriter(String fileName) throws IOException{
         BufferedWriter x = new BufferedWriter(new FileWriter(fileName));
 		return x;
 	}
 
+	/**
+	 * Checks if the given file exists
+	 * 
+	 * @param filename
+	 * @return
+	 */
 	public static boolean fileExists(String filename) {
 		return new File(filename).exists();
 	}
 	
+	/**
+	 * Replaces the file part of a full path with a new file name and returns 
+	 * the modified full path
+	 * 
+	 * @param fullPath
+	 * @param newFilename
+	 * @return
+	 */
 	public static String replaceFilename(String fullPath, String newFilename){
 		int lastIndex = fullPath.lastIndexOf("/");
 		if (lastIndex == -1)
@@ -42,6 +80,10 @@ public class FileUtil {
 			return fullPath.substring(0,lastIndex+1).concat(newFilename);
 	}
 	
+	/**
+	 * Test Unit, can be changed
+	 * @param args
+	 */
 	public static void main (String args[]){
 		System.out.println(replaceFilename("/res/public/slashie.gig", "santi.jpg"));
 	}
