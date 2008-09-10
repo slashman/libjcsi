3c488d296c13925a9025f29111e8454298a8d3b6
eben.howard <eben.howard@997be215-1356-0410-a312-4b11603a79ba>
2008-09-10 19:47:20 +0000

    Modified textcomponents to use CSIColor as the default, with backwards support for int implementations.  Modified CSIColor to include a static array COLOR_ARRAY that is the default palette.  Moved various CSIColor, int, and String comparison methods to CSIColor instead of WSwingConsoleInterface. (This last simply so that textcomponents could use them)

--- src/net/slashie/libjcsi/CSIColor.java
+++ src/net/slashie/libjcsi/CSIColor.java
@@ -1,6 +1,6 @@
 package net.slashie.libjcsi;
 
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class CSIColor {
 
@@ -8,33 +8,11 @@ public class CSIColor {
     private int value;
     public static final CSIColor BLACK = new CSIColor(0x000000),  BLUE = new CSIColor(0x0000ff),  BROWN = new CSIColor(0x808000),  CYAN = new CSIColor(0x00ffff),  DARK_BLUE = new CSIColor(0x0000c8),  DARK_GRAY = new CSIColor(0X404040),  DARK_RED = new CSIColor(0x800000),  GRAY = new CSIColor(0x808080),  GREEN = new CSIColor(0x008000),  LEMON = new CSIColor(0x00FF00),// duplicated as LIGHT_GREEN for useability
          LIGHT_GRAY = new CSIColor(0xc0c0c0),  LIGHT_GREEN = new CSIColor(0x00FF00),  MAGENTA = new CSIColor(0xff00ff),  ORANGE = new CSIColor(0xffc800),  PINK = new CSIColor(0xffafaf),  PURPLE = new CSIColor(0x800080),  TEAL = new CSIColor(0x008080),  RED = new CSIColor(0xff0000),  WHITE = new CSIColor(0xffffff),  YELLOW = new CSIColor(0xffff00);
-
-    /**
-     * Returns a vector containing all of the constants from this class.
-     * Should only be called once and assigned to a variable in your program to keep object creation at a minimum.
-     * 
-     * @return v Vector<CSIColor>
+    /*
+     *colorArray holds the default palette.  Any new items need to be added to the end
+     * in order to maintain backwards compatability 
      */
-    static final public Vector<CSIColor> getColorVector() {
-        Vector<CSIColor> v = new Vector<CSIColor>(16);
-        v.add(BLACK);
-        v.add(DARK_BLUE);
-        v.add(GREEN);
-        v.add(TEAL);
-        v.add(DARK_RED);
-        v.add(PURPLE);
-        v.add(BROWN);
-        v.add(LIGHT_GRAY);
-        v.add(GRAY);
-        v.add(BLUE);
-        v.add(LEMON);
-        v.add(CYAN);
-        v.add(RED);
-        v.add(MAGENTA);
-        v.add(YELLOW);
-        v.add(WHITE);
-        return v;
-    }
+    public static CSIColor[] COLOR_ARRAY = {BLACK, DARK_BLUE, GREEN, TEAL, DARK_RED, PURPLE, BROWN, LIGHT_GRAY, GRAY, BLUE, LEMON, CYAN, RED, MAGENTA, YELLOW, WHITE};
 
     public CSIColor(int pr, int pg, int pb, int pa) {
         this((pa << 24) | (pr << 16) | (pg << 8) | pb);
@@ -103,4 +81,79 @@ public class CSIColor {
         value -= x;
         value += a;
     }
+
+    public int getColor(String colorName) {
+        ArrayList<CSIColor> tempList = new ArrayList<CSIColor>();
+        for (int i = 0; i < COLOR_ARRAY.length; i++) {
+            tempList.add(COLOR_ARRAY[i]);
+        }
+        if (colorName == null) {
+            return -1;
+        }
+        if (colorName.equals("BLACK")) {
+            return tempList.indexOf(BLACK);
+        }
+        if (colorName.equals("DARK_BLUE")) {
+            return tempList.indexOf(DARK_BLUE);
+        }
+        if (colorName.equals("GREEN")) {
+            return tempList.indexOf(GREEN);
+        }
+        if (colorName.equals("TEAL")) {
+            return tempList.indexOf(TEAL);
+        }
+        if (colorName.equals("DARK_RED")) {
+            return tempList.indexOf(DARK_RED);
+        }
+        if (colorName.equals("PURPLE")) {
+            return tempList.indexOf(PURPLE);
+        }
+        if (colorName.equals("BROWN")) {
+            return tempList.indexOf(BROWN);
+        }
+        if (colorName.equals("LIGHT_GRAY")) {
+            return tempList.indexOf(LIGHT_GRAY);
+        }
+        if (colorName.equals("GRAY")) {
+            return tempList.indexOf(GRAY);
+        }
+        if (colorName.equals("BLUE")) {
+            return tempList.indexOf(BLUE);
+        }
+        if (colorName.equals("LEMON")) {
+            return tempList.indexOf(LEMON);
+        }
+        if (colorName.equals("CYAN")) {
+            return tempList.indexOf(CYAN);
+        }
+        if (colorName.equals("RED")) {
+            return tempList.indexOf(RED);
+        }
+        if (colorName.equals("MAGENTA")) {
+            return tempList.indexOf(MAGENTA);
+        }
+        if (colorName.equals("YELLOW")) {
+            return tempList.indexOf(YELLOW);
+        }
+        if (colorName.equals("WHITE")) {
+            return tempList.indexOf(WHITE);
+        }
+        return -1;
+    }
+
+    public CSIColor getColorFromCode(int code) {
+        if ((code < 0) || (code > COLOR_ARRAY.length - 1)) {
+            return BLACK;
+        }
+        return COLOR_ARRAY[code];
+    }
+
+    public int getCodeFromColor(CSIColor color) {
+        ArrayList<CSIColor> tempList = new ArrayList<CSIColor>();
+        for (int i = 0; i < COLOR_ARRAY.length; i++) {
+            tempList.add(COLOR_ARRAY[i]);
+        }
+        return tempList.indexOf(color);
+    }
 }
+
