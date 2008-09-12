97e481dc745ae22c41d9983a1e1bfb0e519e4e78
eben.howard <eben.howard@997be215-1356-0410-a312-4b11603a79ba>
2008-09-12 03:04:31 +0000

    meant to test for errors refreshing

---
+++ src/net/slashie/libjcsi/examples/luck/toybox/RefreshTest.java
@@ -0,0 +1,53 @@
+package net.slashie.libjcsi.examples.luck.toybox;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import net.slashie.libjcsi.CSIColor;
+import net.slashie.libjcsi.wswing.WSwingConsoleInterface;
+
+public class RefreshTest {
+
+    WSwingConsoleInterface mainInterface;
+    Random rng = new Random();
+
+    public RefreshTest() {
+        ArrayList<CSIColor> list = new ArrayList<CSIColor>();
+        for (int i = 0; i < CSIColor.FULL_PALLET.length; i++) {
+            list.add(CSIColor.FULL_PALLET[i]);
+        }
+
+        Collections.sort(list);
+
+        try {
+            mainInterface = new WSwingConsoleInterface("CSIColor Test", false);
+        } catch (ExceptionInInitializerError eiie) {
+            System.out.println("Fatal Error Initializing Swing Console Box");
+            eiie.printStackTrace();
+            System.exit(-1);
+        }
+        int x = 0, times = 0;
+        Random rng = new Random();
+
+        CSIColor tempColor = CSIColor.WHITE, backColor = CSIColor.BLACK;
+
+        do {
+            for (int k = 0; k < mainInterface.ydim; k++) {
+                for (int i = 0; i < mainInterface.xdim; i++) {
+                    tempColor = list.get(rng.nextInt(list.size()));
+                    backColor = list.get(rng.nextInt(list.size()));
+                    
+                    mainInterface.print(i, k, 'Q', tempColor, backColor);
+                }
+            }
+            mainInterface.refresh();
+//            mainInterface.cls();
+            
+        } while (true);
+
+    }
+
+    public static void main(String[] args) {
+        new RefreshTest();
+    }
+}
