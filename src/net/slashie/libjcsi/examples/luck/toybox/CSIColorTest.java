a13ad780ab03fc95f73cb6d2996fc936468a04de
eben.howard@gmail.com <eben.howard@gmail.com@997be215-1356-0410-a312-4b11603a79ba>
2009-04-28 16:08:46 +0000

    Adds a function .toString() to CSIColor in order to make use of the extended pallet easier.
    Also added CSIColorComparison which can be used to preview colors.

--- src/net/slashie/libjcsi/examples/luck/toybox/CSIColorTest.java
+++ src/net/slashie/libjcsi/examples/luck/toybox/CSIColorTest.java
@@ -15,18 +15,19 @@ public class CSIColorTest {
 
     public CSIColorTest() {
         try {
-            mainInterface = new WSwingConsoleInterface("CSIColor Test", false);
+            mainInterface = new WSwingConsoleInterface("CSIColor Test");
         } catch (ExceptionInInitializerError eiie) {
             System.out.println("Fatal Error Initializing Swing Console Box");
             eiie.printStackTrace();
             System.exit(-1);
         }
         int xdiv = 255 / 80, ydiv = 255 / 25;
-        
+
         CSIColor tempColor;
+        tempColor = CSIColor.HAN_PURPLE;
         for (int i = 0; i < mainInterface.xdim; i++) {
             for (int k = 0; k < mainInterface.ydim; k++) {
-               tempColor = new CSIColor(i * xdiv, k * ydiv, (i * xdiv + k * ydiv) / 2);
+                tempColor = new CSIColor(i * xdiv, k * ydiv, (i * xdiv + k * ydiv) / 2);
                 mainInterface.print(i, k, "#", new CSIColor(tempColor));
             }
         }
